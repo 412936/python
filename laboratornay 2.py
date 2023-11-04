642c1ebb954911e378a5d49cd95962ca40ea57de
druzh_inina <druzhinina-2005@mail.ru>
2023-11-05 00:03:05 +0300

    Add files via upload

---
+++ laboratornay 2.py
@@ -0,0 +1,54 @@
+#Дружинина Дарья R3142 412936
+import csv
+from random import randin
+#Задание№1
+with open('books.csv', 'r', encoding='windows-1251') as csvfile:
+    table = csv.reader(csvfile, delimiter=';')
+    flag = 0
+    for row in table:
+        if len(list(row)[1]) > 30:
+            flag += 1
+    print(flag)
+
+#Задание№2
+flag = []
+author = input('Введите автора :\n')
+with open('books.csv', 'r', encoding='windows-1251') as csvfile:
+    book = csv.reader(csvfile, delimiter=';')
+    for row in book:
+        if (list(row)[4] == author or list(row)[3] == author) and (float(list(row)[7]) >= 150):
+                flag.append(list(row)[1])
+    print('Книги автора, цена которых 150 рублей и выше:\n')
+    for books in flag:
+        print(books)
+
+#Задание№3
+result = [randint(1, 9411) for x in range(20)]
+flag = []
+with open('books.csv', encoding='windows-1251') as csvfile:
+    book = [i for i in csv.reader(csvfile, delimiter = ';')]
+    for i in result:
+        reader_list = list(book)
+    for i in flag:
+        row = reader_list[i]
+    with open('resultat.txt', 'w') as csvfile:
+        for i in range(1,21):
+            csvfile.writelines(str(i) + ' ' + book[i][3] + '. ' + book[i][1] + ' - ' + book[i][6] + '\n')
+
+#Доп задания
+tag = []
+inf = []
+with open('books-en.csv', 'r') as csvfile:
+    line = csvfile.readline()
+    for row in csvfile:
+        book = row.split(';')
+        tag.append(book[4])
+        name = str(book[1])
+        downloads = int(book[-2])
+        inf += [[name, downloads]]
+    inf.sort(key = lambda k:k[1], reverse = True)
+    print('Теги:', set(tag))
+    print('20 самых популярных книг:')
+    for i in range(20):
+        print(f'{i + 1}. {inf[i][0]}')
+

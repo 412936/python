41528546141952e819ea5b187d32e265d8e868e6
druzh_inina <druzhinina-2005@mail.ru>
2023-12-01 19:42:39 +0300

    Update laba3.py

--- laba3.py
+++ laba3.py
@@ -1,58 +1,61 @@
 #Дружинина Дарья R3142 412936
 import tkinter as tk
-import random
-import string
+from random import randint
 
 def close():
     window.destroy()
-    
+def key(m,M):
+    letters_list = '0123456789abcdefghijklmnopqrstuvwxyz'
+    len(letters_list)
+    s=0
+    code = ''
+    codes = ''
+    for i in range(3):
+        while True:
+            a = randint(0, len(letters_list)-1)
+            if (a+s)<M:
+                s += a
+                code += str(a)
+                codes += letters_list[a]
+                break
+    while True:
+        a=randint(0, len(letters_list)-1)
+        if ((a+s)>m) and ((a+s)<M):
+            s += a
+            code += str(a)
+            codes += letters_list[a]
+            break
+    return(codes)
 def generate_key():
-    key = ''
-    block_sum = 0
-    block_size = 0
-
-    while len(key) < 15:
-        # Сгенерируем случайный символ
-        symbol = random.choice(string.ascii_uppercase + string.digits)
-
-        # Получим весовой коэффициент для символа
-        weight = get_symbol_weight(symbol)
-
-        # Проверим, если добавление символа не превышает заданный интервал
-        if block_sum + weight <= 9:
-            key += symbol
-            block_sum += weight
-            block_size += 1
-
-        # Если добавление символа приводит к превышению заданного интервала, то сбросим блок
-        else:
-            block_sum = 0
-            block_size = 0
-
-    # Форматируем ключ в требуемый формат
-    formatted_key = f"{key[:5]}-{key[5:9]}-{key[:4]}"
-    key_field.delete(0, tk.END)
-    key_field.insert(0, formatted_key)
-    
-def get_symbol_weight(symbol):
-    return 1
+    global answer
+    letters_list = '0123456789abcdefghijklmnopqrstuvwxyz'
+    blok1 = ''
+    for i in range(5):
+        blok1 += letters_list[randint(0, len(letters_list)-1)]
+    blok1 += '-'
+    blok2 = ''
+    for i in range(4):
+        blok2 += letters_list[randint(0, len(letters_list)-1)]
+    blok2 += '-'
+    blok3 = key(30,120)
+    answer = blok1 + blok2 + blok3
+    canvas.itemconfig(key_canvas, text=answer)
 
 window = tk.Tk()
-window.geometry('750x422')
-bg_img = tk.PhotoImage(file='mario.png')
+canvas = tk.Canvas(window, width=750, height=422)
+canvas.pack()
 
-lbl_bg = tk.Label(window, image=bg_img)
-lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
+image = tk.PhotoImage(file="mario.png")
+canvas.create_image(0, 0, anchor=tk.NW, image=image)
 
 frame = tk.Frame(window)
 frame.place(relx=0.5, rely=0.5, anchor='center')
-
-key_field = tk.Entry(window, font=("Arial", 16))
-key_field.pack(pady=150)
 
 btn_calc = tk.Button(frame, text='Calculate', command=generate_key)
 btn_calc.grid(column=0, row=3, padx=10, pady=15)
 btn_exit = tk.Button(frame, text='Cancel', command=close)
 btn_exit.grid(column=2, row=3, padx=10, pady=15)
+
+key_canvas = canvas.create_text(375, 50, text="Генерация ключа", fill="red", font=('Arial 30 bold'))
 
 window.mainloop()
